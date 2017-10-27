195c2e6c19a1f6440b83fbdbd66ea1c327891b04
Unknown <dantedg@comcast.net>
2017-10-27 14:23:42 -0700

    Fixed Tables

--- Tables.sql
+++ Tables.sql
@@ -3,29 +3,42 @@ DROP TABLE users;
 DROP TABLE books;
 
 CREATE TABLE users(
-    id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
+    id INT NOT NULL AUTO_INCREMENT,
+    person_name VARCHAR(50),
     is_teacher BOOLEAN,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE books(
-    id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(50),
     author VARCHAR(50),
-    popularity VARCHAR(20),
-    length VARCHAR(20),
+    lexile VARCHAR(20),
+    page_length VARCHAR(20),
     genre VARCHAR(20),
     trait1 VARCHAR(30),
     trait2 VARCHAR(30),
+    recommended INT,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE chosen(
-    id int NOT NULL AUTO_INCREMENT,
-    user_id int,
-    book_id int,
+    user_id INT,
+    book_id INT,
+    recommended BOOLEAN,
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(book_id) REFERENCES books(id),
-    PRIMARY KEY(id) 
+    PRIMARY KEY(user_id,book_id) 
 );
+
+--Teacher Page
+SELECT books.title, books.author, books.lexile, books.page_length, books.genre, books.trait1, books.trait2, books.recommended 
+    FROM books;
+
+--Teacher Add
+INSERT INTO books VALUE();
+
+--Student Page
+SELECT books.title, books.author, books.lexile, books.page_length, books.genre, books.trait1, books.trait2, books.recommended
+    FROM books,chosen,users WHERE (chosen.user_id = "person" AND books.id = chosen.book_id); 
+
