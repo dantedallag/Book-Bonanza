7c237a4ce44e0318fec9e3464a891d6ec7af02fb
Patterson Jaffurs <khosumet@gmail.com>
2017-11-05 22:16:28 -0800

    started on searching query

---
+++ studentSearch.php
@@ -0,0 +1,20 @@
+<?php
+$host = "dbserver.engr.scu.edu";
+$user = "ddallaga";
+$password = "00001033223";
+$database = "sdb_ddallaga";
+$port = 3306;
+$connection = mysqli_connect($host, $user, $password, $database)
+	or die("Error: " . mysqli_error($connection));
+if(!$connection) {
+	echo "Error connecting";
+}
+//Query for lexile, author(optional), and recommendations?
+$result = mysqli_query($connection,"SELECT title, author, lexile, page_length, genre, trait1, trait2, recommended ".
+								   "FROM books ".
+								   "WHERE ");
+//Pass result set onto searchResults and parse the remainder?
+
+mysqli_close($connection);
+
+?>
