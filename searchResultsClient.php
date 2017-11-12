18616f143d0c52428ae7e5cbcaf3ee1f007db5e7
Patterson Jaffurs <khosumet@gmail.com>
2017-11-12 01:30:17 -0800

    algorithm mostly done

--- searchResultsClient.php
+++ searchResultsClient.php
@@ -23,6 +23,19 @@
 ?>
 <form class="buttons">
     <input type="button" class="btn btn-default" value="Choose Books!" onclick="window.location.href='studentHome.html'"/>
+	<script>
+        $("#submit").click(function() {
+			//Holds IDs of books to add to relational table
+			var data = new Array(10);
+			
+			var url = "searchResultsServerSubmit.php";
+			$.post(url, data, function(res) {
+				var newurl = 'http://linux.students.engr.scu.edu/~ddallaga/htdocs/studentHome.php';
+				console.log(res);
+				window.location.href = newurl;
+			});
+		});
+	</script>
 </form>
 </body>
 
--- searchResultsServer.php
+++ searchResultsServer.php
@@ -16,8 +16,57 @@
 		echo "Error connecting";
 		return;
 	}
+
 	$resultSet = mysqli_query($connection, 'SELECT title, author, lexile, page_length, genre, trait1, trait2, recommended FROM books');
-	$avgRating = mysqli_query($connection, 'SELECT AVG(recommended) FROM books');
+	$avgRating = mysqli_query($connection, 'SELECT AVG(recommended) avg FROM books');
+	$thing = mysqli_fetch_assoc($avgRating);
+	$avg = $thing['avg'];
+	
+	if(mysqli_num_rows($resultSet) > 0){
+		$resultArray = array();
+		while($row = mysqli_fetch_array($resultSet){
+			$score = 0.0;
+			if($row['lexile'] <= $lexile)
+				$score += 1.0;
+			else if($row['lexile'] <= ($lexile + 150))
+				$score += 0.8;
+			else if($row['lexile'] <= ($lexile + 300))
+				$score += 0.6;
+			else if($row['lexile'] <= ($lexile + 450))
+				$score += 0.4;
+			else
+				$score += 0.2;
+			
+			if($row['author'] === $author)
+				$score += 0.84;
+			
+			if($row['recommended'] >= $avg)
+				$score += 0.68;
+			//Need to know how length is done
+			if($length > 0){
+				if($row['page_length'] <= ($length + 25) || $row['page_length'] >= ($length - 25))
+					$score += 0.52;
+			}
+			
+			if($row['genre'] === $genre)
+				$score += 0.36;
+			
+			if(row['trait1'] === $trait1 || row['trait1'] === $trait2)
+				$score += 0.1;
+			if(row['trait2'] === $trait1 || row['trait2'] === $trait2)
+				$score += 0.1;
+			//Change this value as needed
+			if($score < 1.0)
+				continue;
+			$row['score'] = $score;
+			$resultArray[] = $row;
+		}
+	}
+	//Should sort the array by sub associative arrays by the 'score' key
+	usort($resultArray, function ($itemA, $itemB){
+		return $itemB['score'] <=> $itemA['score'];
+	});
+	
 	echo "<table class='table table-striped' align='center'>
 		<thead>
 		  <tr>
@@ -42,8 +91,21 @@
 			<td>Bad</td>
 			<td>3</td>
 		  </tr>";
-
+	//Should print the recommended books
+	for($i = 0;$i < 10, $i < $resultArray.count();$i++){
+		echo "<r>
+				<td id = 'row".$i."'><input type='checkbox'></td>";
+		echo "<td>".$resultArray[$i]['title']."</td>";
+		echo "<td>".$resultArray[$i]['author']."</td>";
+		echo "<td>".$resultArray[$i]['lexile']."L</td>";
+		echo "<td>".$resultArray[$i]['page_length']."</td>";
+		echo "<td>".$resultArray[$i]['trait1']."</td>";
+		echo "<td>".$resultArray[$i]['trait2']."</td>";
+		echo "<td>".$resultArray[$i]['recommendations']."</td>";
+	}
 
 	echo "</tbody>
 		</table>";
+		
+	mysqli_close($connection);
 ?>
