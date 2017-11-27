d68f1fcdfcb6cb845a28d0e6c47485858a5e55c5
Isaac Jorgensen <ijorgensen@scu.edu>
2017-11-27 15:58:44 -0800

    Validation Complete Student Search

--- studentSearch.php
+++ studentSearch.php
@@ -45,7 +45,7 @@ function logOut() {
 		<div class="form-group1" style=bl>
 			<label for "sel1">Author</label>
 			<input type="text" onblur="authorFilter()" style="width: 200px" class="form-control" id="sel1"/>
-            <script>
+            <!--<script>
                 function authorFilter() {
                     var x = document.getElementById("sel1").value;
                     console.log(x);
@@ -53,7 +53,7 @@ function logOut() {
                     if(!x.match(Exp))
                         alert("You must enter an author's name.")
                 }
-            </script>
+            </script>-->
 		</div>
 		<div class="form-group1" style=bl>
 			<label for "sel2">Lexile Level(Required)</label>
@@ -69,20 +69,19 @@ function logOut() {
 			<div class="col-xs-3 selectContainer">
 				<label for "sel4">Genre</label>
 				<select class="form-control" id="sel4">
-					<option selected='selected' disabled value="">Choose Genre</option>
-					<option value="Historical Fiction">Historical Fiction</option>
-					<option value="Children's Literature">Children's Literature</option>
-					<option value="Fantasy">Fantasy</option>
-					<option value="Novel">Novel</option>
-					<option value="Young Adult">Young Adult</option>
-					<option value="Realistic Fiction">Realistic Fiction</option>
-					<option value="Fiction">Fiction</option>
-					<option value="Adventure">Adventure</option>
-					<option value="Romance">Romance</option>
-					<option value="Middle Grade">Middle Grade</option>
-					<option value="Science Fiction">Science Fiction</option>
-					<option value="Mystery">Mystery</option>
-					<option value="Horror">Horror</option>
+                    <option selected='selected' value="Historical Fiction">Historical Fiction</option>
+                    <option value="Children's Literature">Children's Literature</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Novel">Novel</option>
+                    <option value="Young Adult">Young Adult</option>
+                    <option value="Realistic Fiction">Realistic Fiction</option>
+                    <option value="Fiction">Fiction</option>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Romance">Romance</option>
+                    <option value="Middle Grade">Middle Grade</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Mystery">Mystery</option>
+                    <option value="Horror">Horror</option>
 					<option value="Biography">Biography</option>
 					<option value="Picture Book">Picture Book</option>
 				</select>
@@ -90,37 +89,36 @@ function logOut() {
 		</div>
 		<div class="form-group22">
            	<div class="col-xs-3 selectContainer">
-				<label for "sel5">1st Protagonist Trait</label>
+				<label for "sel5">Protagonist Trait 1</label>
 				<select class="form-control" id="sel5">
-					<option selected='selected' disabled value="">Choose First Trait</option>
-                    <option value="">African American</option>
-                    <option value="">Native American</option>
-                    <option value="">Mexican American</option>
-                    <option value="">Scandinavian American</option>
-                    <option value="">Hispanic</option>
-                    <option value="">Chilean</option>
-                    <option value="">Pakistani</option>
-                    <option value="">Haitian</option>
-                    <option value="">Polish</option>
-                    <option value="">Persian</option>
-                    <option value="">Afghanistani</option>
-                    <option value="Long">Interracial</option>
-                    <option value="Short">Biracial</option>
-                    <option value="Long">Young</option>
-                    <option value="Medium">Dog</option>
-					<option>Male</option>
-					<option>Female</option>
-					<option>Couple</option>
-					<option>Animal</option>
+                    <option selected='selected' value="Short">African American</option>
+                    <option value="Native American">Native American</option>
+                    <option value="Mexican American">Mexican American</option>
+                    <option value="Scandinavian American">Scandinavian American</option>
+                    <option value="Hispanic">Hispanic</option>
+                    <option value="Chilean">Chilean</option>
+                    <option value="Pakistani">Pakistani</option>
+                    <option value="Haitian">Haitian</option>
+                    <option value="Polish">Polish</option>
+                    <option value="Persian">Persian</option>
+                    <option value="Afghanistani">Afghanistani</option>
+                    <option value="Interracial">Interracial</option>
+                    <option value="Biracial">Biracial</option>
+                    <option value="Young">Young</option>
+                    <option value="Dog">Dog</option>
+					<option value="Male">Male</option>
+					<option value="Female">Female</option>
+					<option value="Dog">Couple</option>
+					<option value="Dog">Animal</option>
 				</select>			
 			</div>
            	<div class="col-xs-3 selectContainer">
-				<label for "sel6">2nd Protagonist Trait</label>
+				<label for "sel6">Protagonist Trait 2</label>
 				<select class="form-control" id="sel6">
-					    <option selected='selected' disabled value="">Choose Second Trait</option>
-		                <option value="Male">Male</option>
-		                <option value="Female">Female</option>
-	                    <option value="Couple">Couple</option>
+                    <option selected='selected' value="">Choose Second Trait</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                    <option value="Couple">Couple</option>
 				</select>				
 			</div>
 		</div>
@@ -129,6 +127,11 @@ function logOut() {
         <button type="button" class="btn btn-default" id="submit">Submit Selections</button>
 		<script>
 		$("#submit").click( function() {
+            var lex = document.getElementById("sel2").value;
+            if($.trim(lex).length == 0){
+                alert("You must enter a Lexile Level.");
+                return;
+            }
 			var author = $("#sel1").val();
 			var lexile = $("#sel2").val();
 			if(lexile == ""){
